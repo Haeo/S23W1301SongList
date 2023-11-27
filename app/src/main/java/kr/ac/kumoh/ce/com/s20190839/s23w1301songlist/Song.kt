49a893ca56c4c299c0cad5f57750e630835bac07
Haeo <eltsacbig63@gmail.com>
2023-11-27 19:20:43 +0900

    🚀 SongList 완성

---
+++ app/src/main/java/kr/ac/kumoh/ce/com/s20190839/s23w1301songlist/Song.kt
@@ -0,0 +1,9 @@
+package kr.ac.kumoh.ce.com.s20190839.s23w1301songlist
+
+data class Song(
+    val id: Int,
+    val title: String,
+    val singer: String,
+    val rating: Int,
+    val lyrics: String
+)
